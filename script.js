d15fa6c00a489e4b0e52f1bf9742cd9442faaad4
Devin Beck <devin6beck@gmail.com>
2022-02-08 18:08:55 -0700

    add comments

--- script.js
+++ script.js
@@ -1,15 +1,31 @@
-const container = document.querySelector('.container');
-const btnGray = document.createElement('button');
-const btnRest = document.createElement('button');
-
+// create variables that point to the HTML elements
+const container = document.getElementById('gridContainer');
 const topButtonContainer = document.getElementById("topButtonContainer");
 const buttonsContainer = document.querySelector('.buttons');
 
+// create variables that point to newly created button elements
+const btnGray = document.createElement('button');
+const btnRest = document.createElement('button');
+
+// put text in the btnReset
 btnRest.textContent = 'Rest';
-// Keep 10px betweeen topButtonContainer and the container. 
+
+// add 10px to the bottom margin of the topButtonContainer div and then 
+// add the btnRest to the topButtonContainer.
 topButtonContainer.style.marginBottom = '10px';
 topButtonContainer.appendChild(btnRest).classList.add('btn');
 
+// Start the page off with a 16 by 16 grid.
+createDivs(16, 16);
+
+grayColor();
+
+
+btnRest.addEventListener('click', reset);
+
+
+
+// this function created the grid
 function createDivs(col, rows) {
   for(let i  = 0; i < (col * rows); i++) {
     const div = document.createElement('div');
@@ -21,7 +37,6 @@ function createDivs(col, rows) {
   }
 }
 
-createDivs(16, 16);
 
 function grayColor() {
   const boxs = container.querySelectorAll('.box');
@@ -38,10 +53,6 @@ function grayColor() {
   buttonsContainer.appendChild(btnGray).classList.add('btn');
 }
 
-grayColor();
-
-
-btnRest.addEventListener('click', reset);
 
 function reset() {
   let newSize = prompt("How big would you like the grid? Choose 1-100");
